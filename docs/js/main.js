236f2ef3695a4c07127a603d47f2261e8714cdea
yuran710 <130776468+yuran710@users.noreply.github.com>
2023-06-15 07:14:06 +0300

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  var mixer = mixitup('.directions__list')
+
+  $('.directions__filter-btn').on('click',function name() {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+  $(this).addClass('directions__filter-btn--active')
+  })
+  $(".rateYo").rateYo({
+  });
+
+
+
+
+
+})
