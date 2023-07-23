da391737791d11e14ddead5b2e6e41e854888eb5
yuran710 <130776468+yuran710@users.noreply.github.com>
2023-07-23 13:16:44 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -24,7 +24,18 @@ $(function () {
     $('.customers__slider').slick('slickNext')
   })
 
-
+  $('.questions__acc-link').on('click', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('questions__acc-link--active')) {
+      $(this).removeClass('questions__acc-link--active')
+      $(this).children('.questions__acc-text').slideUp()
+    } else {
+      $('.questions__acc-link').removeClass('questions__acc-link--active')
+      $('.questions__acc-text').slideUp()
+      $(this).addClass('questions__acc-link--active')
+      $(this).children('.questions__acc-text').slideDown()
+    }
+  })
 
 
 
