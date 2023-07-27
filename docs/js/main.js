b7bd6f407aa6baf88a059aad636ed944fcd76156
yuran710 <130776468+yuran710@users.noreply.github.com>
2023-07-27 08:17:15 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -38,20 +38,30 @@ $(function () {
   })
   function init() {
     let map = new ymaps.Map('map', {
-      center: [40.71862775342618,-73.99846535532649],
-      zoom:12.4
-  });
-  
-  map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
-  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-}
-  
+      center: [40.71862775342618, -73.99846535532649],
+      zoom: 12.4
+    });
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  }
   ymaps.ready(init);
+
+  $(".header__nav-list, .footer__nav-list, body").on("click", "a", function (e) {
+    e.preventDefault()
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 800)
+  })
+
+
+
+
 
 })
